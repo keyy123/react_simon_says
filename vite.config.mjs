200c5332d508ddb655ea8e6375fbfcf543b67b70
keyy123 <parktwins1@gmail.com>
2024-06-14 09:49:04 -0400

    vite is completely installed...

--- vite.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import React from "@vitejs/plugin-react";
-
-export default defineConfig({
-    plugins: [React()],
-    root: "./public"
-    // root: "/react_simon_says/"
-})
